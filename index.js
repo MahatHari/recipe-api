f343fc908b869f9e05beeceb379c55998bfd8b60
hari mahat <hari.mahat@siili.com>
2023-06-08 16:59:32 +0300

    trying options

--- index.js
+++ index.js
@@ -6,23 +6,19 @@ const server= jsonServer.create();
 const router=jsonServer.router("db.json");
 const middleWare= jsonServer.defaults();
 
-const domainList=["https://mellifluous-eclair-5bf6e9.netlify.app"]
-
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (domainList.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-  }
-
 
 const port= process.env.port || 3001;
 
 server.use(router);
 server.use(middleWare);
+server.use(cors({
+    origin:true,
+    credentials:true,
+    preflightContinue:false,
+    methods:"GET,HEAD,PUT,POST,DELETE",
+}))
+
+server.options('*',cors());
 server.use(cors())
 
-server.listen(port);
+server.listen(port,()=>console.log("Server is listening"));
