3ff4f8b14f04b2f2e2ccb10d66204359d6813850
hari mahat <hari.mahat@siili.com>
2023-06-08 17:07:51 +0300

    trying options

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ const server= jsonServer.create();
 const router=jsonServer.router("db.json");
 const middleWare= jsonServer.defaults();
 
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 
 const port= process.env.port || 3001;
 
-server.use(router);
-server.use(middleWare);
-server.use(cors({
-    origin:true,
-    credentials:true,
-    preflightContinue:false,
-    methods:"GET,HEAD,PUT,POST,DELETE",
-}))
 
-server.options('*',cors());
-server.use(cors())
+server.use(middleWare);
+server.use(router);
 
 server.listen(port,()=>console.log("Server is listening"));
